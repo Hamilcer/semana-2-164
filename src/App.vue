e5b48ff2a69df0bd54094993ced47e63ff4a6b4b
Hamilcer <68626128+Hamilcer@users.noreply.github.com>
2020-12-05 18:57:22 -0500

    team

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app" class="container d-flex justify-content-between mt-5">
+
+    <div v-for="(item, index) of member" :key="index">
+      <TeamCard v-bind:member="item"></TeamCard>
+    </div>
+    
+
+  </div>
+</template>
+
+<script>
+
+import TeamCard from './components/TeamCard.vue'
+export default {
+  name: 'App',
+  components: {
+    TeamCard
+  },
+  data() {
+    return{
+      member: [
+      {
+        codigo: 1,
+        nombre: 'Camilo Andrés Cipagauta',
+        descripcion: 'Estudiante de la Universidad Distrital Francisco José de Caldas, Bogotá-Colombia. ',
+        rol: 'Desarrollador Backend',
+        image: './img/camilo.png'
+      },
+      {
+        codigo: 2,
+        nombre: 'Juan David Gonzáles',
+        descripcion: 'Estudiante de Ingeniería Informática Universidad Autónoma de Occidente, 21 años, Cali- Colombia.',
+        rol: 'Desarrollador Backend- Tester',
+        image: './img/juan.png'
+      },
+      {
+        codigo: 3,
+        nombre: 'Hamilton Camilo Espitia',
+        descripcion: 'Estudiante Desarrollo de software Misión TIC 2022 - UTP, 18 años, Bogotá-Colombia.',
+        rol: 'Desarrollador Frontend',
+        image: './img/hamilton.png' 
+      },
+      {
+        codigo: 4,
+        nombre: 'Angie Carolina Bonilla',
+        descripcion: 'Ingeniera industrial de la universidad distrital, 23 años, Bogotá-Colombia.',
+        rol: 'Desarrollador Frontend',
+        image: './img/angie.png'
+      },
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+}
+</style>
+
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,39 @@
+<template>
+  
+    <div class="ml-2 mr-2 border borderlight rounded">
+        <img v-if="member.image" :src="member.image" alt="Imagen del miembro" class="img rounded-top" >
+        
+            <h3 class="text-center" >{{ member.nombre }}</h3>
+            <p> {{member.descripcion}} </p>
+            <p> {{member.rol}} </p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'TeamCard',
+        props: ['member']
+    }
+</script>
+
+<style scoped>
+div {
+    width: 262.4px;
+}
+div:hover {
+    transition: all 0.10s ease-in-out 0s;
+    zoom: 100.2%;
+    box-shadow: rgba(0, 0, 0, 0.15) 2px 4px 25px 4px;
+}
+h3 {
+    padding: 10px;
+}
+p {
+    padding: 5px;
+}
+.ml-2 {
+    color: black;
+}
+
+</style>
