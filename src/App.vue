e5ee799b8313393ac8ee7b95e030c1cd79e528cb
acbonillap <acbonillap@correo.udistrital.edu.co>
2020-12-05 20:59:33 -0500

    agrege carpeta node_modules

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+  <banner-carousel></banner-carousel>
+  <section-footer></section-footer>
   </div>
 </template>
 
 <script>
 
+import BannerCarousel from './components/BannerCarousel.vue';
+
+import SectionFooter from './components/SectionFooter.vue';
+
 export default {
   name: 'App',
-}
+  components: {
+    BannerCarousel,
+    SectionFooter
+  },
+  }
 </script>
-
